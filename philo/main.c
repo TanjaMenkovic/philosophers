ceea807acb8ae349a27b8e4be20852e7bb8c3e01
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-09 16:05:39 +0300

    Update main.c

--- philo/main.c
+++ philo/main.c
@@ -15,30 +15,24 @@
 static int	check_args(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
-		return (error_msg("incorrect number of arguments!\n", 1));
+		return (error_msg("incorrect number of arguments!\n"));
 	if (ft_atoi(argv[1]) < 1)
-		return (error_msg("needed at least 1 philosopher!\n", 1));
+		return (error_msg("needed at least 1 philosopher!\n"));
 	if (ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0 || ft_atoi(argv[4]) < 0
 		|| (ft_atoi(argv[5]) < 0 && argc == 6))
-		return (error_msg("input must be positive numbers!\n"), 1);
+		return (error_msg("input must be positive numbers!\n"));
 }
 
-int	main(int argc, char **argv)
+static int allocate_and_init(t_philo *p, phtread_mutex_t *forks, t_locks *l, char **argv)
 {
-	t_philo			*p;
-	pthread_mutex_t	*forks;
-	t_locks			l;
-
-	if (check_args(argc, argv) == 1)
-		return (1);
 	p = malloc(sizeof(t_philo) * ft_atoi(argv[1]));
 	if (!p)
-		return (error_msg("Failed to malloc philosophers!\n", 1));
-	forks = malloc(sizeof(pthread_mutex_t) * ft_atoi(argv[1]));
+		return (error_msg("failed to malloc philosophers!\n"));
+	forks = malloc(sizeof(pthread_mutex_t) * ft_atoi(p->argv[1]));
 	if (!forks)
 	{
 		free(p);
-		return (error_msg("Failed to malloc forks!\n", 1));
+		return (error_msg("failed to malloc forks!\n"));
 	}
 	if (init_locks(&l, p, forks) != 0)
 	{
@@ -52,7 +46,20 @@ int	main(int argc, char **argv)
 		free(forks);
 		return (1);
 	}
-	init_philos(p, forks, &l, argc, argv);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_philo			*p;
+	pthread_mutex_t	*forks;
+	t_locks			l;
+
+	if (check_args(argc, argv) == 1)
+		return (1);
+	if (allocate_and_init(p, forks, &l, argv) != 0)
+		return (1);
+	p = init_philos(p, forks, &l, argv);
 
 	return (0);
 }
