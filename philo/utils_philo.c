1c34e8efe4c0a7cc59e45c7c373cc6200c0340fc
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-09 16:07:27 +0300

    Update utils_philo.c

--- philo/utils_philo.c
+++ philo/utils_philo.c
@@ -17,7 +17,7 @@ int	get_current_time(void)
 	t_timeval	time;
 
 	if (gettimeofday(&time, NULL) == -1)
-		error_msg("[gettimeofday ERROR]\n", 1);
+		error_msg("gettimeofday\n");
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
@@ -29,4 +29,13 @@ void	print_action(t_philo *p, char *action)
 	time = get_current_time() - p->born_time;
 	printf(GREEN"[%ld]"RESET" %d%s\n", time, p->rang_num, action);
 	pthread_mutex_unlock(p->mutex.write_lock);
+}
+
+void	ft_usleep(int mls)
+{
+	int	start;
+
+	start = get_current_time();
+	while (get_current_time() - start < mls)
+		usleep(500);
 }
