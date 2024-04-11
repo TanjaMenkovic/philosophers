8eb6becf80f56710ec8e4167166f61a7aa2a6840
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-11 16:42:35 +0300

    Update utils_philo.c

--- philo/utils_philo.c
+++ philo/utils_philo.c
@@ -12,7 +12,7 @@
 
 #include "philo.h"
 
-/* It uses gettimeofday() to get the current time in microseconds and then 
+/* get_current_time function uses gettimeofday() to get the current time in microseconds and then 
 converts it to milliseconds before returning the result. */
 int	get_current_time(void)
 {
@@ -23,6 +23,10 @@ int	get_current_time(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
+/* print_action function prints an action performed by a philosopher (p) along with a timestamp. 
+It acquires a lock on the write mutex to ensure thread safety while printing, gets the current 
+time using get_current_time(), calculates the time difference between the current time and the 
+philosopher's born_time, and then prints the action along with the timestamp. */
 void	print_action(t_philo *p, char *action)
 {
 	size_t	time;
@@ -33,6 +37,10 @@ void	print_action(t_philo *p, char *action)
 	pthread_mutex_unlock(p->mutex.write_lock);
 }
 
+/* ft_usleep function mimics the behavior of usleep() but in milliseconds. It gets the current time 
+before entering a loop where it repeatedly checks if the elapsed time since start (obtained using 
+get_current_time()) is less than the specified mls (milliseconds). Inside the loop, it calls usleep(500) 
+to pause the execution for a short duration. This loop continues until the desired milliseconds have passed. */
 void	ft_usleep(int mls)
 {
 	int	start;
