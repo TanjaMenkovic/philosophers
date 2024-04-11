be2933a4714a8346ec6fe02f9a8df97f0c842b15
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-11 16:56:54 +0300

    Update simulation.c

--- philo/simulation.c
+++ philo/simulation.c
@@ -12,7 +12,7 @@
 
 #include "philo.h"
 
-/* checking if all philos have finished eating the required num of meals */
+/* is_all_eaten function is checking if all philos have finished eating the required num of meals */
 static int	is_all_eaten(t_philo *p)
 {
 	int		finished;
@@ -38,13 +38,13 @@ static int	is_all_eaten(t_philo *p)
 	return (0);
 }
 
-/* observer thread that continuously monitors the state of philosophers. It takes a pointer to a philosopher structure (t_philo *ptr) as input.
-
-Inside an infinite loop, it iterates through each philosopher and checks if any philosopher has exceeded the allowed time without eating (reached the time to die).
-
-If a philosopher is found to have exceeded the allowed time without eating, it prints a message indicating that the philosopher has died, releases the write_lock mutex, and returns NULL to terminate the thread.
-
-If all philosophers have finished eating (as determined by the is_all_eaten function), it returns NULL to terminate the thread. */
+/* is_died function observer thread that continuously monitors the state of philosophers. It takes 
+a pointer to a philosopher structure (t_philo *ptr) as input. Inside an infinite loop, it iterates 
+through each philosopher and checks if any philosopher has exceeded the allowed time without eating 
+(reached the time to die). If a philosopher is found to have exceeded the allowed time without eating, 
+it prints a message indicating that the philosopher has died, releases the write_lock mutex, and 
+returns NULL to terminate the thread. If all philosophers have finished eating (as determined by the 
+is_all_eaten function), it returns NULL to terminate the thread. */
 static void	*is_died(void *ptr)
 {
 	t_philo	*p;
@@ -73,13 +73,13 @@ static void	*is_died(void *ptr)
 	return (NULL);
 }
 
-/* This function represents the routine of a philosopher. It takes a pointer to a philosopher structure (t_philo *philo) as input.
-
-The philosopher first locks the mutex of the left fork, then prints a message indicating that it has taken a fork, and similarly for the right fork.
-
-Then, it locks the meal_lock mutex, prints a message indicating that it is eating, updates the last meal time, increments the number of meals eaten, and unlocks the meal_lock mutex.
-
-It then sleeps for the eating time (p->data.time_to_eat), unlocks both forks, and prints messages indicating that it is sleeping and then thinking. */
+/* philo_simulation function represents the routine of a philosopher. It takes a pointer to a 
+philosopher structure (t_philo *philo) as input. The philosopher first locks the mutex of the 
+left fork, then prints a message indicating that it has taken a fork, and similarly for the 
+right fork. Then, it locks the meal_lock mutex, prints a message indicating that it is eating, 
+updates the last meal time, increments the number of meals eaten, and unlocks the meal_lock mutex. 
+It then sleeps for the eating time (p->data.time_to_eat), unlocks both forks, and prints messages 
+indicating that it is sleeping and then thinking. */
 static void	philo_simulation(t_philo *p)
 {
 	pthread_mutex_lock(p->mutex.left_fork);
@@ -99,11 +99,10 @@ static void	philo_simulation(t_philo *p)
 	print_action(p, " is thinking");
 }
 
-/* This function represents the starting point of the simulation for a philosopher. It takes a pointer to a philosopher structure (t_philo *ptr) as input.
-
-If the philosopher's rang is even, it sleeps for a short time (1 microsecond).
-
-It then initializes the born time and last meal time of the philosopher and enters an infinite loop where it repeatedly executes the philo_simulation function. */
+/* start_simulation function represents the starting point of the simulation for a philosopher.  It takes 
+a pointer to a philosopher structure (t_philo *ptr) as input. If the philosopher's rang is  even, it 
+sleeps for a short time (1 microsecond). It then initializes the born time and last meal  time of the 
+philosopher and enters an infinite loop where it repeatedly executes the philo_simulation function. */
 static void	*start_simulation(void *ptr)
 {
 	t_philo	*p;
@@ -121,14 +120,9 @@ static void	*start_simulation(void *ptr)
 }
 
 /* This function launches the simulation by creating and joining threads for the observer and the philosophers.
-
-It creates a thread for the observer using pthread_create.
-
-It then creates threads for each philosopher using pthread_create.
-
-After creating all threads, it joins the observer thread using pthread_join.
-
-Then, it detaches the threads for each philosopher using pthread_detach. */
+It creates a thread for the observer using pthread_create. It then creates threads for each philosopher using 
+pthread_create. After creating all threads, it joins the observer thread using pthread_join. Then, it detaches 
+the threads for each philosopher using pthread_detach. */
 void	launcher(t_locks *l, int count)
 {
 	pthread_t	obsorver_id;
